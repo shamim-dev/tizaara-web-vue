35e471b6c610727acd6e5582f7b6dbb3edc6f5df
shamim-dev <mrezashamim@gmail.com>
2020-06-02 20:16:27 +0600

    v2 test

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -10,6 +10,7 @@ import {
 
 import NuxtError from '..\\layouts\\error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
+import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '..\\node_modules\\bootstrap\\dist\\css\\bootstrap.css'
 
@@ -75,7 +76,7 @@ export default {
       }
     }, [
       loadingEl,
-
+      h(NuxtBuildIndicator),
       transitionEl
     ])
   },
@@ -200,6 +201,10 @@ export default {
     },
 
     setLayout (layout) {
+      if(layout && typeof layout !== 'string') {
+        throw new Error('[nuxt] Avoid using non-string value as layout property.')
+      }
+
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
